6125011318bd434380fb3de7a60440d052536427
Raven John Lagrimas <rjlagrimas08@gmail.com>
2017-12-03 05:45:14 +0800

    Added bank details page for web

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,7 @@
 const DATETIME_FMT = 'YYYY-MM-DD HH:mm:ss';
 const API = 'http://192.168.0.24:3000';
 const USER_ID = 10002;
+const NAME = 'Ninz';
 
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
@@ -25,7 +26,7 @@ function start () {
         autoclose: true
     });
 
-    console.log('called');
+	$('.modal').modal();
 
     $('.datepicker').pickadate({
         closeOnSelect: true,
@@ -47,6 +48,7 @@ function bind() {
 	}
 
 	$('#offer-form').submit(submitOffer);
+	$('#details-form').submit(submitDetails);
 }
 
 function submitOffer(e) {
@@ -101,11 +103,77 @@ function submitOffer(e) {
 	return false;
 }
 
+function submitDetails(e) {
+
+	const form = e.target;
+
+	const payload = {
+		card_number: form.card_number.value,
+		expiry: form.expiry.value,
+		cvv: form.cvv.value,
+		zip_code: form.zip_code.value
+	};
+
+	$('#submit-details').attr('disabled', 'disabled');
+	$('#submit-details-loader').toggleClass('active');
+
+	function doneLoading() {
+		$('#submit-details').removeAttr('disabled');
+		$('#submit-details-loader').toggleClass('active');
+	}
+
+	$.ajax({
+			method: 'POST',
+			url: API + '/api/details',
+			data: JSON.stringify(payload),
+			contentType: 'application/json',
+			headers: {
+				'x-user-id': USER_ID
+			}
+		})
+		.done(result => {
+			doneLoading();
+			show('listings');
+		})
+		.fail(e => {
+			console.error(e);
+			Materialize.toast('Ooops! Something went wrong. I\'m sorry', 4000)
+			doneLoading();
+		});
+
+	return false;
+}
+
 function show(section) {
-	location.hash = section;
 	$('section').hide();
 	$('#nav-mobile li').removeClass('active');
 	$('#' + section + '-nav').addClass('active');
 	$('.' + section + '-section').show();
+}
+
+function removeRow(row) {
+	const $row = $(row);
+
+	$row.closest('tr').hide();
+
+	b = $row.closest('tbody');
+
+	if ($row.closest('tbody').children(':visible').length === 0) {
+		$('#no-offer-tr').show();
+	} else {
+		$('#no-offer-tr').hide();
+	}
+}
+
+function sendMessage() {
+	const msg = $('#message-area').val();
+
+	if (!msg) {
+		return;
+	}
+
+	$('#message-thread').append('<span>' + NAME + ': </span><span>' + msg + '</span><br/>')
+
+	$('#message-area').val('');
 }
 $(document).ready(start);
